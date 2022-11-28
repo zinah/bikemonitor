0770c8d814e18ee129be799c4e6095e4ad67f47b
Karolina Jelen <jehonala@gmail.com>
2022-11-28 14:51:32 +0100

    Remove fake data from the functions that now contact the real API

--- src/main/kotlin/com/example/bikemonitor/OsloBysykkel.kt
+++ src/main/kotlin/com/example/bikemonitor/OsloBysykkel.kt
@@ -66,96 +66,14 @@ class OsloBysykkel : GeneralBikeShareFeedSpecification {
   }
 
   override fun getBikeStationsData(url: String): BikeStationsJSON {
-    val fakeStationsJSON =
-        """
-    {
-        "last_updated": 1553592653,
-        "data": {
-          "stations": [
-            {  
-              "station_id":"627",
-              "name":"Skøyen Stasjon",
-              "address":"Skøyen Stasjon",
-              "lat":59.9226729,
-              "lon":10.6788129,
-              "capacity":20
-            },
-            {  
-              "station_id":"623",
-              "name":"7 Juni Plassen",
-              "address":"7 Juni Plassen",
-              "lat":59.9150596,
-              "lon":10.7312715,
-              "capacity":15
-            },
-            {  
-              "station_id":"610",
-              "name":"Sotahjørnet",
-              "address":"Sotahjørnet",
-              "lat":59.9099822,
-              "lon":10.7914482,
-              "capacity":20
-            }
-          ]
-        }
-      }
-    """
     val stations = get(url).jsonObject.toString()
     val stationsJson = gson.fromJson(stations, BikeStationsJSON::class.java)
-
-    val fakeStations = gson.fromJson(fakeStationsJSON, BikeStationsJSON::class.java)
-    if (stationsJson == null) {
-      return fakeStations
-    } else {
-      return stationsJson
-    }
+    return stationsJson
   }
 
   override fun getBikeAvailabilityData(url: String): BikeAvailabilityJSON {
-    var fakeAvailabilityJSON =
-        """
-        {
-            "last_updated": 1540219230,
-            "data": {
-              "stations": [
-                {
-                  "is_installed": 1,
-                  "is_renting": 1,
-                  "num_bikes_available": 4,
-                  "num_docks_available": 8,
-                  "last_reported": 1540219230,
-                  "is_returning": 1,
-                  "station_id": "623"
-                },
-                {
-                  "is_installed": 1,
-                  "is_renting": 1,
-                  "num_bikes_available": 7,
-                  "num_docks_available": 5,
-                  "last_reported": 1540219230,
-                  "is_returning": 1,
-                  "station_id": "627"
-                },
-                {
-                  "is_installed": 1,
-                  "is_renting": 1,
-                  "num_bikes_available": 4,
-                  "num_docks_available": 9,
-                  "last_reported": 1540219230,
-                  "is_returning": 1,
-                  "station_id": "610"
-                }
-              ]
-            }
-          }
-        """
     val availability = get(url).jsonObject.toString()
     val availabilityJson = gson.fromJson(availability, BikeAvailabilityJSON::class.java)
-    val fakeAvailability = gson.fromJson(fakeAvailabilityJSON, BikeAvailabilityJSON::class.java)
-    if (availabilityJson == null) {
-      return fakeAvailability
-    } else {
-      return availabilityJson
-    }
+    return availabilityJson
   }
 }
